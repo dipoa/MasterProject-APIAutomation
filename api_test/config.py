eb26713f0300dee6fbf1b70d749eae5f630b931d
Dipo A Supriadi <dipoas@Dipos-MacBook-Pro.local>
2023-08-24 21:33:00 +0100

    add auth token to config.py

--- api_test/config.py
+++ api_test/config.py
@@ -1,1 +1,20 @@
+import requests
+
 BASE_URL = "https://restful-booker.herokuapp.com"
+
+def get_auth_token():
+    endpoint = "/auth"
+    data = {
+        "username": "admin",
+        "password": "password123"
+    }
+    
+    response = requests.post(f"{BASE_URL}{endpoint}", json=data)
+    
+    # Ensure a successful response before proceeding.
+    response.raise_for_status()
+
+    return response.json().get('token', None)
+
+# Generate the token once when the module is imported.
+AUTH_TOKEN = "YWRtaW46cGFzc3dvcmQxMjM="
